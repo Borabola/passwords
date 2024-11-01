40c78d8758d307a3d1bcd42785dffaba376dd273
Olha Sokolova <osokolova1@gmail.com>
2024-11-01 19:56:22 +0100

    Add dictionary lisis

---
+++ src/utils/variables.ts
@@ -0,0 +1,4 @@
+export const UPPERCASE_ABC = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
+export const LOWERCASE_ABC ="abcdefghijklmnopqrstuvwxyz";
+export const SPECIAL_SYMBOLS ="!@#$%^&*()?.<>|=+:;,[-_]";
+export const NUMBERS ="0123456789";
