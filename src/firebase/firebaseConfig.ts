0fbc50496eebfe6d9d1502398ac83b41980e96eb
Olha Sokolova <osokolova1@gmail.com>
2024-11-06 15:57:17 +0100

    Edit firebase deploy rules

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-//import { getDatabase } from "firebase/database";
+import { getDatabase } from "firebase/database";
 //import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	//databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const database = getDatabase(app);
+const database = getDatabase(app);
 // const storage = getStorage(app);
 
 const auth = getAuth(app);
@@ -48,4 +48,4 @@ const auth = getAuth(app);
 }*/
 
 //export { app, auth, database, storage };
-export { app, auth };
+export { app, auth, database};
